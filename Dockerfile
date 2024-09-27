062ad4bd46f08baa78e0b9b6284f074b8ad54c6a
LedruRollin <32680314+LedruRollin@users.noreply.github.com>
2024-09-27 17:52:45 +0200

    [NEW] Add containerization

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.12.6-slim-bookworm
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+ENV APP_HOME=/home/roback
+WORKDIR $APP_HOME
+
+# Install libmagic for python-magic library
+RUN apt-get update && apt-get install -y libmagic1
+
+COPY requirements.txt $APP_HOME/requirements.txt
+
+RUN pip install --no-cache-dir -r $APP_HOME/requirements.txt
+
+COPY ./src $APP_HOME/src
+COPY ./entrypoint.sh $APP_HOME/entrypoint.sh
+
+EXPOSE 8000
+
+ENTRYPOINT $APP_HOME/entrypoint.sh
--- src/search_targets/models/media_model.py
+++ src/search_targets/models/media_model.py
@@ -1,12 +1,13 @@
 import uuid
 
-from django.conf import settings
-from django.db.models import CASCADE
-from django.db.models import FileField
-from django.db.models import ForeignKey
-from django.db.models import Model
-from django.db.models import TextChoices
-from django.db.models.fields import TextField
+from django.db.models import (
+    CASCADE,
+    FileField,
+    ForeignKey,
+    Model,
+    TextChoices,
+    TextField,
+)
 
 from search_targets.models import SearchTarget
 
@@ -19,7 +20,6 @@ class MediaType(TextChoices):
 
 def get_media_path(instance, _):
     """ Return path in filesystem storage """
-    extension = "file"
     if instance.type == MediaType.Image:
         extension = "png"
     elif instance.type == MediaType.Video:
@@ -32,22 +32,9 @@ def get_media_path(instance, _):
     return f"{instance.search_target.id}/{uuid.uuid4()}.{extension}"
 
 
-
 class Media(Model):
     """ Model for a media (image, video, gif, etc)"""
-
     search_target = ForeignKey(SearchTarget, on_delete=CASCADE, related_name="media")
     name = TextField()
     file = FileField(upload_to=get_media_path)
     type = TextField(choices=MediaType.choices, default='')
-
-
-    def save(self, *args, **kwargs):
-        # Set media type
-        content_type = self.file.file.content_type
-        if content_type not in settings.ACCEPTED_MIME_TYPES:
-            raise ValueError(f"Unsupported media type {content_type}")
-        media_type = settings.ACCEPTED_MIME_TYPES[content_type]
-        self.type = MediaType(media_type)
-        super().save(*args, **kwargs)
-
